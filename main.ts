46a46b12c73ba2a00e37ca66e6a4d8c4bbf27371
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2025-04-18 22:46:06 +0200

    feat: added onload function

--- main.ts
+++ main.ts
@@ -1,134 +1,118 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface XitSettings {
+    githubToken: string;
+    gitAutoSync: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: XitSettings = {
+    githubToken: '',
+    gitAutoSync: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Xit extends Plugin {
+    settings: XitSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // Sync git repository at startup if enabled
+        if (this.settings.gitAutoSync) {
+            this.gitSyncAtStartup();
+        }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // Add command to manually sync git repository
+        this.addCommand({
+            id: 'git-sync',
+            name: 'Sync Git repository',
+            callback: () => this.gitSyncAtStartup()
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async gitSyncAtStartup() {
+        try {
+            new Notice('Syncing Git repository...');
+            
+            // Get the path to the vault
+            const vaultPath = (this.app.vault.adapter as any).basePath;
+            
+            // Using Node.js capabilities for desktop version of Obsidian
+            if (typeof require === 'function') {
+                const util = require('util');
+                const exec = util.promisify(require('child_process').exec);
+                
+                // Set Git credentials if token is provided
+                if (this.settings.githubToken) {
+                    // Configure Git to use the token for HTTPS authentication
+                    await exec(`cd "${vaultPath}" && git config --local credential.helper '!f() { echo "username=x-access-token"; echo "password=${this.settings.githubToken}"; }; f'`);
+                }
+                
+                // Execute git commands
+                await exec(`cd "${vaultPath}" && git fetch --all`);
+                await exec(`cd "${vaultPath}" && git reset --hard origin/main`);
+                await exec(`cd "${vaultPath}" && git clean -fd`);
+                
+                new Notice('Git repository synchronized successfully');
+            } else {
+                // For web version, we'd need to implement with isomorphic-git
+                throw new Error('Git operations in web version are not implemented yet');
+            }
+        } catch (error) {
+            console.error('Error synchronizing git repository:', error);
+            new Notice('Error synchronizing git repository: ' + error.message);
+        }
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    onunload() {
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    plugin: Xit;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: Xit) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const {containerEl} = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('GitHub Token')
+			.setDesc('Personal access token for GitHub authentication')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('ghp_xxxxxxxxxxxxxxxxxxxx')
+				.setValue(this.plugin.settings.githubToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.githubToken = value;
 					await this.plugin.saveSettings();
 				}));
-	}
+                
+        new Setting(containerEl)
+            .setName('Auto Sync Git Repository')
+            .setDesc('Automatically synchronize Git repository at startup')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.gitAutoSync)
+                .onChange(async (value) => {
+                    this.plugin.settings.gitAutoSync = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
